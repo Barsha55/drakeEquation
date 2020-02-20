f8969d3b84fd9b29be6d28e17eb8861e538fe191
barsha <barsha.sangraula.2022@students.lhschools.org>
2020-02-20 12:31:10 -0500

    rstar input added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -32,7 +32,19 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
         Scanner keyboard;
         keyboard =new Scanner(System.in);
 
+        //todo initialize the scanner to take input and commit
+        //ask for rStar number store in the rStar variable and commit
+        //ask for f_p fraction store in the f_p variable and commit
+        //ask for numberOfExoplanets store in the numberOfExoplanets variable and commit
+        //ask for f_l fraction store in the f_l variable and commit
+        //ask for f_i fraction store in the f_i variable and commit
+        //ask for f_c fraction store in the f_c variable and commit
+        //ask for l number store in the l variable and commit
 
+
+
+        System.out.println("how many stars are formed in the galaxy per year per year?");
+        rStar= keyboard.nextInt();
     }
 
 }
